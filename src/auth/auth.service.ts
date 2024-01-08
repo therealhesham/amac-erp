06c541ff2288337a91648435a0f59adb45beb504
thereakhesham <heshammoha231992@gmail.com>
2024-01-08 12:08:23 +0200

    commit on master

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,7 +19,7 @@ private  jwtservice : JwtService
      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
     res.header("Access-Control-Allow-Origin", "localhost:5173");
     // res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Allow-Origin", "https://amacerp.onrender.com");
+    // res.header("Access-Control-Allow-Origin", "https://amacerp.onrender.com");
 res.setHeader("ss","ssssssss")
     res.header("Access-Control-Max-Age", "54h");
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,Set-Cookie');
